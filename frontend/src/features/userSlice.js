f3887b5c7e857147a68aed90a9c03e3219c5ddd7
HassanMunene <awanzihassan@gmail.com>
2024-03-25 11:19:23 +0300

    did set up the product and user slice

---
+++ frontend/src/features/userSlice.js
@@ -0,0 +1,28 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+import appApi from "../services/appApi";
+
+initialState = null;
+
+export const userSlice = createSlice({
+    name: "users",
+    initialState,
+    reducers: {
+        logout: () => initialState,
+        addNotification: (state, action) => {
+            state.notifications.unshift(action.payload);
+        },
+        resetNotification: (state) => {
+            state.notifications.forEach((obj) => {
+                obj.status = "read";
+            })
+        },
+    },
+    extraReducers: (builder) => {
+        builder.addMatcher(appApi.endpoints.signup.matchFulfilled, (_, {payload}) => payload);
+        builder.addMatcher(appApi.endpoints.login.matchFulfilled, (_, {payload}) => payload);
+    }
+})
+
+export const { logout, addNotification, resetNotifications } = userSlice.actions;
+export default userSlice.reducer;
