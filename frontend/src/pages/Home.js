3a08e0f02285da200b94dbeae79eb819afd65697
HassanMunene <awanzihassan@gmail.com>
2024-03-26 16:30:15 +0300

    added an axio that fetches product from the db and then instantly updates the products redux stroe

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,17 +1,34 @@
 import React from 'react'
+import {useEffect} from 'react';
 import { Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import categories from '../categories';
 import { LinkContainer } from "react-router-bootstrap";
+import axiosInstance from '../axios';
+import { useDispatch, useSelector } from "react-redux";
+import { updateProducts } from "../features/productSlice";
+import ProductPreview from "../components/ProductPreview";
 import "./Home.css";
 
 function Home() {
+  const dispatch = useDispatch();
+  const products = useSelector((state) => state.products);
+  const lastProducts = products.slice(0, 8);
+  useEffect(() => {
+    axiosInstance.get('/products')
+    .then((responseData) => dispatch(updateProducts(responseData.data)))
+    .catch((error) => console.log("Error fetching products:", error));
+  }, [])
   return (
     <div>
       <div className='home-banner'>Home</div>
       <div className='featured-products-container container mt-4'>
         <h2>Last products</h2>
         {/* last products from backend here*/}
+        {console.log(products)}
+        {products.map((product) => (
+          <ProductPreview {...product}/>
+        ))}
         <div>
           <Link to="/category/all" style={{textAlign: "right", display: "block", textDecoration: "None"}}>
             See more {">>"}
