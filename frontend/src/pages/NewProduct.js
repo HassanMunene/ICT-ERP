c2f0067d8892e4405a4df3454127f6b7f8f86791
HassanMunene <awanzihassan@gmail.com>
2024-03-26 12:44:43 +0300

    handle image uploading using the cloudinary widget feature

--- frontend/src/pages/NewProduct.js
+++ frontend/src/pages/NewProduct.js
@@ -2,6 +2,8 @@ import React from "react";
 import  { useState } from 'react';
 import { Alert, Col, Container, Form, Row, Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { useCreateProductMutation } from "../services/appApi";
+import { MdCancel } from "react-icons/md";
 import "./NewProduct.css";
 
 function NewProduct() {
@@ -12,10 +14,38 @@ function NewProduct() {
     const [images, setImages] = useState([]);
     const [imgToRemove, setImgToRemove] = useState(null);
     const navigate = useNavigate();
+    const [createProduct, { isError, error, isLoading, isSuccess }] = useCreateProductMutation();
 
+
+    function showImageWidget () {
+        const widget = window.cloudinary.createUploadWidget({
+            cloudName: "sultanHamud081",
+            uploadPreset: "iipauyi2",
+        }, (error, result) => {
+            if (!error && result.event === "success") {
+                    setImages((prev) => [...prev, { url: result.info.url, public_id: result.info.public_id }]);
+            }
+        });
+        widget.open();
+    }
 
     function handleSubmit(e) {
         e.preventDefault();
+
+        if (!name || !description || !price || !category || !images.length) {
+            return alert("please fill out all the fields!");
+        }
+        createProduct({name, description, price, category, images})
+        .then(({data}) => {
+            if (data.length > 0) {
+                setTimeout(() => {
+                    navigate('/')
+                }, 1500);
+            }
+        })
+        .catch((error) => {
+            console.log("error creating product:", error.message);
+        }) 
     }
 
     return (
@@ -24,6 +54,8 @@ function NewProduct() {
                 <Col md={6} className="new-product__form--container">
                     <Form style={{ width: "100%" }} onSubmit={handleSubmit}>
                         <h1 className="mt-4">Create a product</h1>
+                        {isSuccess && <Alert variant="success">Product created with succcess</Alert>}
+                        {isError && <Alert variant="danger">{error.data}</Alert>}
                         <Form.Group className="mb-3">
                             <Form.Label>Product name</Form.Label>
                             <Form.Control type="text" placeholder="Enter product name" value={name} required onChange={(e) => setName(e.target.value)} />
@@ -53,16 +85,20 @@ function NewProduct() {
                         </Form.Group>
 
                         <Form.Group className="mb-3">
-                            <Button type="button">
-                                Upload Images
-                            </Button>
+                            <Button type="button" onClick={showImageWidget}>Upload Images</Button>
                             <div className="images-preview-container">
+                                {images.map((image) => (
+                                    <div className="image-preview">
+                                        <img src={image.url} alt="prodImg"/>
+                                        <div className="removeImg" onClick={handleRemoveImg(image)}><MdCancel /></div>
+                                    </div>
+                                ))}
                             </div>
                         </Form.Group>
 
                         <Form.Group>
-                            <Button type="submit">
-                                Create Product
+                            <Button type="submit" disabled={isLoading}>
+                                {isLoading ? "CREATING" : "CREAT PRODUCT"}
                             </Button>
                         </Form.Group>
                     </Form>
