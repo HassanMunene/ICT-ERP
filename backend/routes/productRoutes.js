8455a1b7fab046e890d858d4aacf9a0e41cc6bb3
HassanMunene <awanzihassan@gmail.com>
2024-03-27 13:56:27 +0300

    have added the carts rouutes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -63,12 +63,12 @@ router.delete("/:id", async(req, res) => {
 //route to get a single product
 router.get('/:id', async(req, res) => {
 	const {id} = req.params;
-
+	console.log("requested has arrived");
 	try {
 		const product = await Product.findById(id);
 		//when we get get one product we want to show similar products as well
 		const similar = await Product.find({category: product.category}).limit(5);
-		res.status(200).json(product, similar);
+		res.status(200).json({product, similar});
 	} catch(error) {
 		console.log(error.message);
 		res.status(400).send(error.message);
@@ -77,8 +77,7 @@ router.get('/:id', async(req, res) => {
 
 //retrieve products by the type of category passed
 router.get('/category/:category', async(req, res) => {
-	const {category} = req.params;
-
+    const {category} = req.params;
 	try {
 		let products;
 		const sort = {"_id": -1};
@@ -94,5 +93,90 @@ router.get('/category/:category', async(req, res) => {
 		res.status(400).send(error.message);
 	}
 })
+
+// this route will handle the logic to add product to cart
+router.post('/add-to-cart', async(req, res) => {
+	const {userId, productId, price} = req.body;
+	try {
+		const user = await User.findById(userId);
+		const userCart = user.cart;
+
+		if(user.cart[productId]) {
+			userCart[productId] += 1;
+		} else {
+			userCart[productId] = 1;
+		}
+		userCart.count += 1;
+		userCart.total = Number(userCart.total) + Number(price);
+		user.cart = userCart;
+
+		// make Mongoose recognizes the change and persists it properly.
+		user.markModified('cart');
+		await user.save();
+		res.status(200).json(user);
+	} catch(error) {
+		console.log('Error adding to cart route:', error.message);
+		res.status(400).send(error.message);
+	}
+})
+
+// this route will handle the logic to increase a product from cart
+router.post('/increase-cart', async(req, res)=> {
+  const {userId, productId, price} = req.body;
+
+  try {
+    const user = await User.findById(userId);
+    const userCart = user.cart;
+    userCart.total += Number(price);
+    userCart.count += 1;
+    userCart[productId] += 1;
+    user.cart = userCart;
+    user.markModified('cart');
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+});
+
+
+// this route will handle the logic to decrease a product from cart
+router.post('/decrease-cart', async(req, res) => {
+	const {userId, productId, price} = req.body;
+	try {
+		const user = await User.findById(userId);
+		const userCart = user.cart;
+		userCart.total -= Number(price);
+		userCart.count -= 1;
+		userCart[productId] -= 1;
+		user.cart = userCart;
+		user.markModified('cart');
+		await user.save();
+		res.status(200).json(user);
+
+	} catch(error) {
+		console.log("error decreasing item from cart:", error.message);
+		res.status(400).send(error.message);
+	}
+})
+
+router.post('/remove-from-cart', async(req, res)=> {
+  const {userId, productId, price} = req.body;
+  
+  try {
+    const user = await User.findById(userId);
+    const userCart = user.cart;
+    userCart.total -= Number(userCart[productId]) * Number(price);
+    userCart.count -= userCart[productId];
+    delete userCart[productId];
+    user.cart = userCart;
+    user.markModified('cart');
+    await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+})
+
 
 module.exports = router;
