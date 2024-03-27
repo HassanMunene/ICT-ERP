35cab1617fce4e1e020b50208f00bc979026a39b
HassanMunene <awanzihassan@gmail.com>
2024-03-27 09:53:38 +0300

    working on the frontend

--- frontend/src/pages/CategoryPage.js
+++ frontend/src/pages/CategoryPage.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react';
 import axiosInstance from '../axios';
 import Loading from "../components/Loading";
 import ProductPreview from "../components/ProductPreview";
+import { Col, Container, Row } from "react-bootstrap";
+import Pagination from "../components/Pagination";
 
 function CategoryPage () {
 	const {category} = useParams();
@@ -27,7 +29,7 @@ function CategoryPage () {
 	if (loading) {
 		<Loading/>
 	}
-	const productSearch = products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+	const productsSearch = products.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
 
 	function ProductSearch ({ _id, category, name, pictures }) {
 		return <ProductPreview _id={_id} category={category} name={name} pictures={pictures}/>
@@ -38,6 +40,20 @@ function CategoryPage () {
 			<div className={`pt-3 ${category}-banner-container category-banner-container`}>
 				<h1 className="text-center">{category.charAt(0).toUpperCase() + category.slice(1)}</h1>
 			</div>
+			<div className="filters-container d-flex justify-content-center pt-4 pb-4">
+				<input type="search" placeholder="Search" onChange={(e) => setSearchTerm(e.target.value)}/>
+			</div>
+			{productsSearch.length === 0 ? (
+				<h1>No product to show</h1>
+			): (
+				<Container>
+					<Row>
+						<Col md={{ span: 10, offset: 1}}>
+							<Pagination data={productsSearch} RenderComponent={ProductSearch} pageLimit={1} dataLimit={5} tablePagination={false} />
+						</Col>
+					</Row>
+				</Container>
+			)}
 		</div>
 	)
 }
