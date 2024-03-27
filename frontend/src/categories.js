6c15e0da1169614908bdab73fd5cda9c6bd16a30
HassanMunene <awanzihassan@gmail.com>
2024-03-27 11:10:35 +0300

    have added 3 more categories books, drinks, food

--- frontend/src/categories.js
+++ frontend/src/categories.js
@@ -12,6 +12,19 @@ const categories = [
         name: "Laptops",
         img: "https://images.unsplash.com/photo-1575909812264-6902b55846ad?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80"
     },
+    {
+        name: "Books",
+        img: "https://plus.unsplash.com/premium_photo-1681487732859-c2a780022063?q=80&w=1418&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+    },
+    {
+        name: "Drinks",
+        img: "https://images.unsplash.com/photo-1551024709-8f23befc6f87?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZHJpbmtzfGVufDB8fDB8fHww"
+    },
+    {
+        name: "Food",
+        img: "https://images.unsplash.com/photo-1666181551815-b9adecb24e46?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8a2VueWFuJTIwZm9vZHxlbnwwfHwwfHx8MA%3D%3D"
+    },
+
 ];
 
 export default categories;
