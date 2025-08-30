3e2d1889e4cfb3dbbe4c9e134ec4bd12309a6716
HassanMunene <awanzihassan@gmail.com>
2025-08-30 19:45:53 +0300

    removed the root stlying that was giving the application padding on the dashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 import './App.css'
-import ClientBody from './components/ClientBody';
 import { Sidebar } from './components/layout/Sidebar';
 import { Header } from './components/layout/Header';
 import MainDashboard from './pages/MainDashboard';
@@ -9,19 +8,17 @@ import MainDashboard from './pages/MainDashboard';
 function App() {
   return (
     <Router>
-      <ClientBody>
-        <div className="flex h-screen bg-gray-50">
-          <Sidebar />
-          <div className="flex-1 flex flex-col overflow-hidden">
-            <Header />
-            <main className="flex-1 overflow-auto p-6">
-              <Routes>
-                <Route path="/" element={<MainDashboard />} />
-              </Routes>
-            </main>
-          </div>
+      <div className="flex h-screen bg-gray-50">
+        <Sidebar />
+        <div className="flex-1 flex flex-col overflow-hidden">
+          <Header />
+          <main className="flex-1 overflow-auto p-6">
+            <Routes>
+              <Route path="/" element={<MainDashboard />} />
+            </Routes>
+          </main>
         </div>
-      </ClientBody>
+      </div>
     </Router>
   )
 }
